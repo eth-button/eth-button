4df0137a8f357aaf62e50dab39fa66741ee82987
eth-button <eth-button@users.noreply.github.com>
2018-01-20 00:20:25 -0800

    Refactor blockies code into separate library

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+var fs = require("fs");
+
+var package = fs.readFileSync("package.json", "utf-8");
+package = JSON.parse(package);
 
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: "dist/eth-button.js"
+    filename: "dist/" + package.name + ".js"
   },
   plugins: [new UglifyJSPlugin()]
 };
